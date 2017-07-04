19bb33438aabbec7d09f2f96febc4dc310d13be0
AlienGFX <k.rue@free.fr>
2017-07-04 23:47:13 +0200

    Add new entry for whosend env variable

--- lib/functions/core.env.sh
+++ lib/functions/core.env.sh
@@ -17,7 +17,7 @@ pubkey="${deploydir}/pubkey"
 # mail config
 sendmail="on"
 inbox="AITools-production@rkservices.fr"
-whosend="admin@rkservices.fr"
+whosend="admin@rkservices.fr kevin.lachages@gmail.com"
 
 # git config
 GIT_REPOSITORY="git@github.com:AlienGFX/AITools.git"
